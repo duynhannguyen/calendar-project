8839f9de915d114ca12fd1489d7d9c5387333696
Duy Nhan <duynhannguyenn@gmail.com>
2024-03-08 03:05:24 +0700

    feat: handle submit EventFormModal

--- src/components/EventFormModal/EventFormModal.tsx
+++ src/components/EventFormModal/EventFormModal.tsx
@@ -1,12 +1,12 @@
-import { Fragment, useId, useState } from 'react';
-import { Event } from '../../context/Event';
-import { formatDate } from '../../utils/formatDate';
-import { UnionOmit } from '../../utils/types';
-import Modal, { ModalProps } from '../Modal/Modal';
-import './EventFormModal.css';
-import { EVENT_COLORS } from '../../hooks/useEvents';
+import { FormEvent, Fragment, useId, useRef, useState } from "react";
+import { Event } from "../../context/Event";
+import { formatDate } from "../../utils/formatDate";
+import { UnionOmit } from "../../utils/types";
+import Modal, { ModalProps } from "../Modal/Modal";
+import "./EventFormModal.css";
+import { EVENT_COLORS } from "../../hooks/useEvents";
 type EventFormModalProps = {
-  onSubmit: (event: UnionOmit<Event, 'id'>) => void;
+  onSubmit: (event: UnionOmit<Event, "id">) => void;
 } & (
   | {
       onDelete: () => void;
@@ -15,7 +15,7 @@ type EventFormModalProps = {
     }
   | { onDelete?: never; event?: never; date: Date }
 ) &
-  Omit<ModalProps, 'children'>;
+  Omit<ModalProps, "children">;
 const EventFormModal = ({
   onSubmit,
   onDelete,
@@ -26,27 +26,60 @@ const EventFormModal = ({
   const [selectedColor, setSelectedColor] = useState(
     event?.color || EVENT_COLORS[0]
   );
+  const endTimeRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
   const [isAllDay, setIsAllDay] = useState(event?.allDay || false);
-  const [startTime, setStartTime] = useState(event?.startTime || '');
-  console.log('startTime', startTime);
+  const [startTime, setStartTime] = useState(event?.startTime || "");
   const formId = useId();
   const isNew = event == null;
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const name = nameRef.current?.value;
+    const endTime = endTimeRef.current?.value;
+
+    if (name == null || name === "") {
+      return;
+    }
+    const commonProps = {
+      name,
+      date: date || event?.date,
+      color: selectedColor,
+    };
+    let newEvent: UnionOmit<Event, "id">;
+    if (isAllDay) {
+      newEvent = { ...commonProps, allDay: true };
+    } else {
+      if (
+        startTime == null ||
+        startTime === "" ||
+        endTime == null ||
+        endTime === ""
+      ) {
+        return;
+      }
+      newEvent = { ...commonProps, startTime, endTime, allDay: false };
+    }
+    onSubmit(newEvent);
+    modalProps.onClose();
+  };
   return (
     <Modal {...modalProps}>
       <div>
         <div className="modal-title">
           <div>Add Event</div>
           <small>
-            {formatDate(date || event.date, { dateStyle: 'short' })}
+            {formatDate(date || event.date, { dateStyle: "short" })}
           </small>
           <button className="close-btn" onClick={modalProps.onClose}>
             ×
           </button>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor={`${formId}-name`}>Name</label>
-            <input type="text" id={`${formId}-name`} />
+            <input type="text" id={`${formId}-name`} ref={nameRef} />
           </div>
           <div className="form-group checkbox">
             <input
@@ -72,6 +105,7 @@ const EventFormModal = ({
             <div className="form-group">
               <label htmlFor={`${formId}-end-time`}>End Time</label>
               <input
+                ref={endTimeRef}
                 min={startTime}
                 type="time"
                 id={`${formId}-end-time`}
@@ -103,7 +137,7 @@ const EventFormModal = ({
           </div>
           <div className="row">
             <button className="btn btn-success" type="submit">
-              {isNew ? 'Add' : 'Edit'}
+              {isNew ? "Add" : "Edit"}
             </button>
             {onDelete != null && (
               <button
